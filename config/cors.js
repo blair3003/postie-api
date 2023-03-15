d678842220eb3c42c4470079bf490c99e3c24b8c
Blair Fleming <blair3003.bf@gmail.com>
2023-03-15 09:59:50 +0000

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,13 @@
 const cors = require('cors')
 
 const allowedOrigins = [
+<<<<<<< Updated upstream
 	'http://localhost:5173',
+=======
+	'http://localhost:3000',
+	'http://localhost:5173',
+	'http://127.0.0.1:5173'
+>>>>>>> Stashed changes
 ]
 
 module.exports = cors({
