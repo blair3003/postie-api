a354a12cee2094077bb5422dfc0b30cef5959a78
Blair Fleming <blair3003.bf@gmail.com>
2023-04-24 09:28:45 +0100

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -1,9 +1,7 @@
 const cors = require('cors')
 
 const allowedOrigins = [
-	'http://localhost:3000',
-	'http://localhost:5173',
-	'http://127.0.0.1:5173'
+	'https://postie.onrender.com'
 ]
 
 module.exports = cors({
